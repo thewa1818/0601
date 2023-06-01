381d5d91a5f1cf4e4b8356c3eeb483337db80d50
thewa1818 <zawa.i.0218@gmail.com>
2023-06-01 22:45:35 +0900

    今日のファイル

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-$("#").on("click", function () {});
+console.log(1111);
+
+console.log("js")
+
+let num = 1;
+console.log(num , "ここにあるよ");
+
+console.log(2 + 3, "計算中");
+
+console.log("2 + 3" ,"どうなる？")
+
+
+
+$(".test").on("click", function () {
+
+  $(this).html(num);
+
+});
